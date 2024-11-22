ac828e2ad5a553657853d6b2671a4be3464eb7f5
Milan Godawatta <mgodawat@student.42.fr>
2024-11-22 15:27:33 +0100

    pseudo code for push swap

--- rank02/push_swap/src/main.c
+++ rank02/push_swap/src/main.c
@@ -6,18 +6,41 @@
 /*   By: mgodawat <mgodawat@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 09:25:49 by mgodawat          #+#    #+#             */
-/*   Updated: 2024/11/22 14:56:15 by mgodawat         ###   ########.fr       */
+/*   Updated: 2024/11/22 15:26:57 by mgodawat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
+/* @note - step 1
+Decalre pointers to two linked lists, one for stack_a and one for stack_b
+and initialize the pointers as NULL to avoid undefined behaviour to indicate
+that we are starting with empty stacks */
+
+/* @note - step 2
+Handle input count errors, argument count must be 2 or more, and the second
+input must not be empty, if there's input errors return error */
+
+/* @note - step 3
+if the numbers as argv[i] "MULTIPLE ARGUMENTS" and push them into the
+stack_a, if the numbers are as "SINGLE STRING" argv[1] split the string
+and push them into the stack_a
+
+    -- Handle integer overflows, duplicates, and sytax errors, (input must only
+    contains digits, '+' or '-' signs if errors found, free stack A and return
+    error)
+    -- Check for each input if it's a long integer, if it is convert it into
+    a long integer
+
+if everything is good append the numbers as nodes to the stack_a */
+
+/* @note - step 4
+Check if the stack_a is sorted already, if not sorted implement the sorting 
+algorithms... 
+    -- check for 2 numbers ? if so simply swap the numebrs 
+    -- check for 3 numbers ? if so implement simple sort three algorithm 
+    -- check if the stack has more that 3 numbers ? if so turk algorithm */
+
 int main(int argc, char **argv)
 {
-    /* take the numbers as argcs */
-    int i = 1;
-    if (i < argc) {
-        
-    }
-    /* take the numbers as a single string */
 }
