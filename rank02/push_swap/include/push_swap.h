c6389b75e6084952259ce2e1e7c565144d33f311
Milan Godawatta <mgodawat@student.42.fr>
2024-11-27 15:14:15 +0100

    the structure is incorrect starting fresh

--- rank02/push_swap/include/push_swap.h
+++ rank02/push_swap/include/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: mgodawat <mgodawat@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 10:28:08 by mgodawat          #+#    #+#             */
-/*   Updated: 2024/11/27 14:11:51 by mgodawat         ###   ########.fr       */
+/*   Updated: 2024/11/27 15:13:13 by mgodawat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,29 +32,20 @@ typedef struct s_list
 }					t_list;
 
 /* utility functions */
-void				error_exit(void);
+void				error_exit(const char *error_msg);
 void				free_resources(t_list **stack, char **split);
 int					ft_isspace(char c);
 long				ft_atol(char *str);
 
 /* stack manipulation */
 void				init_stack_null(t_list **stack_a, t_list **stack_b);
-t_list				*split_and_push(char *str, t_list *stack);
-t_list				*create_node(int data);
-t_list				*push_numbers_to_stack(char *token, t_list *stack_a);
 void				print_stack(t_list *stack);
 
 /* argument parsing and validation */
 t_list				*handle_arguments(int argc, char **argv);
-void				validate_and_push(char *token, t_list **stack,
-						char **split);
-bool				check_duplicates(t_list *stack, int num);
-bool				is_valid_input(char *str);
-void				append_node(t_list **stack, int num);
 
 /* stack sorting */
-bool				stack_sorted(t_list *stack);
-void				sort_three(t_list **stack);
+
 void				turk_algorithm(t_list **stk_a, t_list **stk_b);
 
 /* sorting operatings */
