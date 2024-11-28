df63d6162d237b152ad3853fbf40392c25f8b88d
Milan Godawatta <mgodawat@student.42.fr>
2024-11-28 15:05:09 +0100

    update

--- rank02/push_swap/include/push_swap.h
+++ rank02/push_swap/include/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: mgodawat <mgodawat@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 10:28:08 by mgodawat          #+#    #+#             */
-/*   Updated: 2024/11/28 12:06:18 by mgodawat         ###   ########.fr       */
+/*   Updated: 2024/11/28 15:00:53 by mgodawat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,9 +46,17 @@ t_list				*create_node(void *content);
 
 /* Utils */
 void				error_exit(char *error_msg);
+void				free_stack(char **split);
+void				free_resources(t_stack *stack, char **split);
+long				ft_atol(const char *str);
 
 /* Handle Arguments */
 void				handle_arguments(int argc, char **argv, t_stack *stack_a);
 void				validate_and_push(char **numbers, t_stack **stack_a);
+int					is_valid_number(const char *str);
+int					add_to_stack(t_stack *stack, int num);
+int					is_duplicate_in_stack(t_stack *stack, int num);
+void				insert_node_to_stack(t_stack *stack, t_list *new_node);
+t_list				*create_stack_node(int num);
 
 #endif
--- rank02/push_swap/src/utils/tools2.c
+++ rank02/push_swap/src/utils/tools2.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   init.c                                             :+:      :+:    :+:   */
+/*   tools2.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mgodawat <mgodawat@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/28 11:36:00 by mgodawat          #+#    #+#             */
-/*   Updated: 2024/11/28 12:10:58 by mgodawat         ###   ########.fr       */
+/*   Updated: 2024/11/28 14:59:04 by mgodawat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,4 +20,28 @@ void	initialize_stacks(t_stack *stack_a, t_stack *stack_b)
 	stack_b->bgn_list = NULL;
 	stack_b->last = NULL;
 	stack_b->list_size = 0;
+}
+
+long	ft_atol(const char *str)
+{
+	long	result;
+	int		sign;
+
+	result = 0;
+	sign = 1;
+	while (*str == ' ' || *str == '\t' || *str == '\n' || *str == '\r'
+		|| *str == '\v' || *str == '\f')
+		str++;
+	if (*str == '-' || *str == '+')
+	{
+		if (*str == '-')
+			sign = -1;
+		str++;
+	}
+	while (*str >= '0' && *str <= '9')
+	{
+		result = result * 10 + (*str - '0');
+		str++;
+	}
+	return (result * sign);
 }
