809c4735a42d3ebd1a55057868a5156c6d5ad422
Milan Godawatta <mgodawat@student.42.fr>
2024-12-02 11:04:23 +0100

    swapping operations

--- rank02/push_swap/src/commands/op_swap.c
+++ rank02/push_swap/src/commands/op_swap.c
@@ -6,17 +6,37 @@
 /*   By: mgodawat <mgodawat@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/01 20:31:08 by milan-godaw       #+#    #+#             */
-/*   Updated: 2024/12/02 09:51:45 by mgodawat         ###   ########.fr       */
+/*   Updated: 2024/12/02 11:03:48 by mgodawat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
 /* sa (swap a): Swap the first 2 elements at the top of the stack a. Does nothing if there is only one or none. */
-void	sa(t_stack *stack, bool print)
+void	sa(t_stack *stack_a, bool print)
 {
-	if (stack->size < 2)
+	if (!stack_a || stack_a->size < 2)
 		return ;
-	t_stack_node *first = stack->head;
-	t_stack_node *second = first->next;
+	swap_nodes(stack_a->head, stack_a->head->next);
+	if (print)
+		ft_putstr("sa\n");
+}
+
+/* sb (swap b) : Swap the first 2 elements at the top of the stack b. Does nothing if there is only one or none. */
+void	sb(t_stack *stack_b, bool print)
+{
+	if (!stack_b || stack_b->size < 2)
+		return ;
+	swap_nodes(stack_b->head, stack_b->head->next);
+	if (print)
+		ft_putstr("sb\n");
+}
+
+/* ss : sa and sb at the same time. */
+void	ss(t_stack *a, t_stack *b, bool print)
+{
+	sa(a, false);
+	sb(b, false);
+	if (print)
+		write(1, "ss\n", 3);
 }
