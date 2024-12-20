d9521d14c92d81c7020892dceea480a3a3498335
Milan Godawatta <mgodawat@student.42.fr>
2024-12-20 17:55:32 +0100

    final update

--- rank02/push_swap/include/push_swap.h
+++ rank02/push_swap/include/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: mgodawat <mgodawat@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/20 14:32:34 by mgodawat          #+#    #+#             */
-/*   Updated: 2024/12/20 14:36:19 by mgodawat         ###   ########.fr       */
+/*   Updated: 2024/12/20 17:55:29 by mgodawat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,10 +19,6 @@
 # include <stdlib.h>
 # include <unistd.h>
 
-/**
- * Stack Node Structure
- * Contains node value and metadata for sorting operations
- */
 typedef struct s_stack_node
 {
 	int					value;
@@ -36,24 +32,18 @@ typedef struct s_stack_node
 	struct s_stack_node	*prev;
 }						t_stack_node;
 
-/**
- * Input Processing Functions
- */
+/* Input Processing Functions */
 char					**split_input(char *str, char separator);
 int						validate_number_syntax(char *number_str);
 bool					check_duplicate_value(t_stack_node *stack, int number);
 
-/**
- * Memory Management Functions
- */
+/* Memory Management Functions */
 void					deallocate_string_array(char **array);
 void					handle_error_and_exit(t_stack_node **stack_a,
 							char **array, bool is_split_input);
 void					deallocate_stack(t_stack_node **stack);
 
-/**
- * Stack Initialization Functions
- */
+/* Stack Initialization Functions */
 void					initialize_stack(t_stack_node **stack_a, char **input,
 							bool is_split_input);
 void					initialize_node_metadata(t_stack_node *stack_a,
@@ -63,9 +53,7 @@ void					calculate_move_costs(t_stack_node *stack_a,
 							t_stack_node *stack_b);
 void					mark_cheapest_moves(t_stack_node *stack_b);
 
-/**
- * Stack Utility Functions
- */
+/* Stack Utility Functions */
 void					add_node(t_stack_node **stack, int value);
 t_stack_node			*get_last_node(t_stack_node *stack);
 t_stack_node			*find_min_node(t_stack_node *stack);
@@ -75,33 +63,25 @@ int						get_stack_size(t_stack_node *stack);
 void					complete_rotation(t_stack_node **stack,
 							t_stack_node *node, char stack_id);
 
-/**
- * Sorting Algorithm Functions
- */
+/* Sorting Algorithm Functions */
 void					sort_three_elements(t_stack_node **stack_a);
 void					sort_five_elements(t_stack_node **stack_a,
 							t_stack_node **stack_b);
 void					sort_stack(t_stack_node **stack_a,
 							t_stack_node **stack_b);
-
-/**
- * Stack Operations - All operations have checker parameter to control output
- */
+/* Stack Operations */
 void					swap_a(t_stack_node **stack_a, bool is_checker);
 void					swap_b(t_stack_node **stack_b, bool is_checker);
 void					swap_both(t_stack_node **stack_a,
 							t_stack_node **stack_b, bool is_checker);
-
 void					push_to_a(t_stack_node **stack_a,
 							t_stack_node **stack_b, bool is_checker);
 void					push_to_b(t_stack_node **stack_b,
 							t_stack_node **stack_a, bool is_checker);
-
 void					rotate_a(t_stack_node **stack_a, bool is_checker);
 void					rotate_b(t_stack_node **stack_b, bool is_checker);
 void					rotate_both(t_stack_node **stack_a,
 							t_stack_node **stack_b, bool is_checker);
-
 void					reverse_rotate_a(t_stack_node **stack_a,
 							bool is_checker);
 void					reverse_rotate_b(t_stack_node **stack_b,
