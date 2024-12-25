26729a3674d9ff1fbc5739744bece92f4c3a06c9
Milan Shanuka Godawatta Liyanage <mgodawat@made-f0Ar3s11.clusters.42paris.fr>
2024-12-25 21:13:11 +0100

    opening a window: update

--- rank02/fract-ol/src/main.c
+++ rank02/fract-ol/src/main.c
@@ -6,7 +6,7 @@
 /*   By: mgodawat <mgodawat@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/23 14:58:10 by mgodawat          #+#    #+#             */
-/*   Updated: 2024/12/24 18:52:59 by mgodawat         ###   ########.fr       */
+/*   Updated: 2024/12/25 21:12:48 by mgodawat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,17 @@
 
 static int	control_arguments(int argc, char **argv)
 {
+	size_t	len;
+
 	if (argc != 2)
 	{
 		ft_putstr_fd("Usage: ./fractol <type>\nTypes: mandelbrot, julia\n",
 			STDERR_FILENO);
 		return (1);
 	}
-	if (ft_strncmp(argv[1], "mandelbrot", ft_strlen(argv[1]))
-		&& ft_strncmp(argv[1], "julia", ft_strlen(argv[1])))
+	len = ft_strlen(argv[1]);
+	if ((len != 10 || ft_strncmp(argv[1], "mandelbrot", 10) != 0) && (len != 5
+			|| ft_strncmp(argv[1], "julia", 5) != 0))
 	{
 		ft_putstr_fd("Invalid fractal type\n", STDERR_FILENO);
 		return (1);
@@ -32,11 +35,13 @@ static int	control_arguments(int argc, char **argv)
 int	main(int argc, char *argv[])
 {
 	t_vars	*vars;
+	char	*program_name;
 
+	program_name = ft_strjoin(argv[0], "-");
 	vars = NULL;
 	if (control_arguments(argc, argv) == 1)
 		exit(EXIT_FAILURE);
-	vars = open_window(ft_strjoin(argv[0], argv[1]));
+	vars = open_window(ft_strjoin(program_name, argv[1]));
 	close_window(vars);
 	return (0);
 }
